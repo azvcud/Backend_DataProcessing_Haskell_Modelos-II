8557f9177add4c2ec047a6e92923f67b0ec961e7
azvcud <azvanegasc@udistrital.edu.co>
2025-01-31 22:38:31 -0500

    Actualización de configuraciones
    Configuraciones nuevas para funcionamiento del proyecto

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,21 @@
-module Main where
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+
+import Web.Scotty
+import Data.Aeson (ToJSON)
+import GHC.Generics (Generic)
+import Data.Text.Lazy (Text)
+
+-- Definición del tipo de dato
+data Response = Response
+    { message :: Text
+    , status  :: Text
+    } deriving (Generic)
+
+instance ToJSON Response
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = scotty 3000 $ do
+    get "/json" $ do
+        let jsonResponse = Response "Hello, world!" "success"
+        json jsonResponse
